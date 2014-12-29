b4e4c0e9918981ccbcaa5af24ce7f2b040bcb7e3
Chris Handley <github.20.alabama3@spamgourmet.com>
2014-12-29 07:17:29 +0000

    finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
 
 
+  get 'users/new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -11,6 +13,7 @@ Rails.application.routes.draw do
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
+  get 'signup'  => 'users#new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
