68a4f34ec73d85b8afec134e049cf7a32abc110c
Chris Handley <github.20.alabama3@spamgourmet.com>
2014-12-29 08:21:55 +0000

    added password hash with validation and test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: 'password', password_confirmation: 'password')
   end
 
   test 'should be valid' do
@@ -51,6 +52,11 @@ class UserTest < ActiveSupport::TestCase
     duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
+  end
+
+  test 'password should have a minimum length' do
+    @user.password = @user.password_confirmation = 'a'*5
+    assert_not @user.valid?
   end
 end
 
