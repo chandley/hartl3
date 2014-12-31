287aa1c931cca38f5e0ac5cffdc17315c9773134
Chris Handley <github.20.alabama3@spamgourmet.com>
2014-12-31 07:28:13 +0000

    added sessions, change flash message to flash.now

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ it gives practice with constructing a rails application
 issues
 
 testing enviroment is minitest - not sure if this will cause issues
+
+##progress
+
+at listing 8.8. may have an issue with login on correct credentials
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
 
 
+  get 'sessions/new'
+
   get 'users/new'
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -14,6 +16,9 @@ Rails.application.routes.draw do
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   get 'signup'  => 'users#new'
+  get 'login'   => 'sessions#new'
+  post 'login'  => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 
   resources :users
 
